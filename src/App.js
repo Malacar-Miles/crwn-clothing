eac42640457257402ea72ae9c8f9f1401c9634e4
Malacar-Miles <105152910+Malacar-Miles@users.noreply.github.com>
2022-05-30 00:51:11 +0300

    Implemented some basic navigation features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shop/hats" element={<HatsPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+const HatsPage = () => (
+  <div><h1>HATS</h1></div>
+);
 
 export default App;
